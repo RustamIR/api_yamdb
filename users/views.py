0356203dd55aab0bc112efed82b5462b291ee75f
Savva <savva.manylov@bk.ru>
2020-09-29 19:21:07 +0300

    S1

---
+++ users/views.py
@@ -0,0 +1,31 @@
+from rest_framework import viewsets, serializers, filters, generics, status
+from users.models import User
+from api.serializers import UserSerializer
+from api.permissions import AdminPermissions
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = AdminPermissions
+
+    @action(detail=True, methods=['PATCH', 'GET'], url_name='user_profile',
+            permission_classes=(IsAuthenticated,))
+    def my_profile(self, request,):
+        serializer = UserSerializer(request.user,
+                                    data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class AuthInfoEmail(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+
+
+class AuthInfoToken(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
